8c3bd85f7b808824101070df383b953bdd76d3cc
mahdin-22 <mahdi.nateghi1993@gmail.com>
2023-12-17 17:07:56 +0330

    Made a Better Repository!

---
+++ README.md
@@ -0,0 +1,17 @@
+# Learning Java with Danial_Germany!
+
+This repository is the main repository for projects which are done as exercises in Java course taught by Danial Jalalnoori.
+I will grow here in Java, just sit back and watch! ;)
+
+Hello World! 2023/11/02
+---------------------------------
+My first Java app was uploaded here on Nov 2nd, 2023 at 21:54. Let's do this!!! :D
+
+Hello World! 2023/12/17
+---------------------------------
+Hello World updated.
+
+Phone Book! 2023/12/17
+---------------------------------
+We were asked to make an app with 3 options: 1) Add new entry 2) Print whole phone book 3) Exit program.
+I added another option to delete an entry and I almost spend 7-8 hours on it. It's OK but whenever "deleteContact" method is called, 4-option menu is printed twice.
---
+++ src/com/learning/helloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.learning.helloWorld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Application Started!");
+        System.out.println("--------------------");
+        System.out.println("---- MENU ----");
+        System.out.println("1. Print Default Message");
+        System.out.println("2. Get the Name and Say Hello");
+        System.out.println("Select an Item:");
+        String userInput = scanner.nextLine();
+        if (userInput.equals("1")) {
+            System.out.println("Hello World!");
+        }
+        if (userInput.equals("2")) {
+            System.out.println("Please Enter Your Name:");
+            String username = scanner.nextLine();
+            System.out.println("Hello " + username);
+        }
+        scanner.close();
+    }
+}
